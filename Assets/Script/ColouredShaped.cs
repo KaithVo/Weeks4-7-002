8dd3ed4e4568b4935078fc0d2f6a31834048ecad
mymacbook <vokar@sheridancollege.ca>
2026-06-01 17:55:54 -0400

    ChangeSprite working but not showing ?

--- Assets/Script/ColouredShaped.cs
+++ Assets/Script/ColouredShaped.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class ColouredShaped : MonoBehaviour
 
 {
+    public List<Sprite> sprites; 
     public SpriteRenderer spriteRenderer;
+    public int spriteIndex; 
+
     public float t;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if( sprites.Count > 0)
+        {
+            //use that to set the sprite
+            spriteRenderer.sprite = sprites[spriteIndex]; 
+        }
     }
 
     // Update is called once per frame
@@ -16,6 +27,14 @@ public class ColouredShaped : MonoBehaviour
     {
         t += Time.deltaTime;
         spriteRenderer.color = Random.ColorHSV(); 
+
+        //creat condition if anykey is down
+
+        if (Input.anyKeyDown)
+        {
+            spriteIndex +=1 ; 
+        }
+
 
     }
 }
