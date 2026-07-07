5cf02542054e87be328558f309dec81b3a5ee0d7
vokar <vokar@sheridancollege.ca>
2026-07-07 15:39:05 -0400

    Week9 created

---
+++ Assets/Script/Explorer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Explorer : MonoBehaviour
+{
+    public float health;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToMove = Vector3.zero;
+
+        //This is the constructor for Vector3
+        directionToMove = new Vector3(0, 0, 0);
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            directionToMove.x -= 1f;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            directionToMove.x += 1f;
+        }
+
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            directionToMove.y += 1f;
+        }
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            directionToMove.y -= 1f;
+        }
+
+        transform.position += directionToMove * speed * Time.deltaTime;
+    }
+}
